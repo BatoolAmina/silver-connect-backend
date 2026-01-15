99f8f0561812111c1a408b1c33ad5563ad70a8d8
Batool Amina <batool.amina.110@gmail.com>
2026-01-15 23:38:13 +0530

    removed service gmail

--- routes/auth.js
+++ routes/auth.js
@@ -227,7 +227,6 @@ router.post('/forgot-password', async (req, res) => {
         const resetUrl = `${process.env.FRONTEND_URL}/reset-password/${resetToken}`;
 
         const transporter = nodemailer.createTransport({
-            service: 'gmail',
             host: 'smtp.gmail.com',
             port: 465,
             secure: true,
